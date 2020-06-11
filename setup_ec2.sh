9588242204a56498d940e7ea814b61280892269e
Nathan Miles <nmiles@stsci.edu>
2020-06-11 12:02:48 -0400

    Updates setup script

--- setup_ec2.sh
+++ setup_ec2.sh
@@ -12,14 +12,13 @@ fi
 ec2_ip=$1  # first arg is IP address of instance
 keyname=$2
 aws_dir=~/.aws/
-git_credentials=~/.ssh/
+ssh_credentials=~/.ssh/
 aws_ssh_key=$aws_dir$keyname
 
 # Copy files credentials to AWS
 scp -ri "$aws_ssh_key" $aws_dir ec2-user@$ec2_ip:~
-scp -ri "$aws_ssh_key" $git_credentials ec2-user@$ec2_ip:~
-
-scp -ri "$aws_ssh_key" ~/aws_setup ec2-user@$ec2_ip:~
+scp -ri "$aws_ssh_key" $ssh_credentials ec2-user@$ec2_ip:~
+scp -ri "$aws_ssh_key" ~/aws_setup/install_miniconda3_linux.sh ec2-user@$ec2_ip:~
 
 # log in to the instance
 ssh -i "$aws_ssh_key" ec2-user@$ec2_ip
